24fd9da450a77bb1cf453d13146142c593a92eab
jellingson <joshua.jonathan.ellingson@gmail.com>
2019-03-17 17:26:45 -0600

    Added maze object and implemented basic stack for use later.

--- maze.js
+++ maze.js
@@ -1,3 +1,5 @@
+const CELL_SIZE = 100;
+
 function Cell(row, col) {
 	this.pos = {
 		row: row,
@@ -12,22 +14,26 @@ function Cell(row, col) {
 	}
 
 	this.size = {
-		width: "100px",
-		height: "100px"
+		width: CELL_SIZE + "px",
+		height: CELL_SIZE + "px"
 	}
+
+	this.visited = false;
 }
 
-Cell.prototype.draw_cell = function() {
+Cell.prototype.drawCell = function() {
 	let div = document.createElement("div");
 
 	div.style.float = "left";
 	div.style.boxSizing = "border-box"
 
-	div = this.draw_cell_walls(div);
-	div = this.draw_cell_size(div);
+	div = this.drawCellWalls(div);
+	div = this.drawCellSize(div);
+
+	return div;
 }
 
-Cell.prototype.draw_cell_walls = function(div) {
+Cell.prototype.drawCellWalls = function(div) {
 	const border_style = "2px solid black"
 	if (this.walls.north === true) {
 		div.style.borderTop = border_style;
@@ -45,11 +51,54 @@ Cell.prototype.draw_cell_walls = function(div) {
 	return div;
 }
 
-Cell.prototype.draw_cell_size = function(div) {
+Cell.prototype.drawCellSize = function(div) {
 	div.style.width = this.size.width;
 	div.style.height = this.size.height;
 
 	return div;
 }
 
-first_cell = new Cell(0, 0);
+function Maze(width, height) {
+	this.width = width;
+	this.height = height;
+	this.cells = [];
+}
+
+Maze.prototype.generateCells = function() {
+	let i, j, cell, rowOfCells = [];
+	let container = document.getElementById('maze');
+
+	while(container.lastElementChild) {
+		container.removeChild(container.lastElementChild);
+	}
+
+	container.style.width = CELL_SIZE*this.width +"px";
+	container.style.height = CELL_SIZE*this.height +"px";
+
+	for(i = 0; i < this.height; i++) {
+		for(j = 0; j < this.width; j++) {
+			cell = new Cell(i, j);
+			container.appendChild(cell.drawCell());
+			rowOfCells[j] = cell;
+		}
+		this.cells.push(rowOfCells)
+	}
+}
+
+function Stack() {
+	this.items = [];
+}
+
+Stack.prototype.push = function(item) {
+	this.items.push(item);
+}
+
+Stack.prototype.pop = function() {
+	if(this.array.length === 0) {
+		return -1;
+	}
+
+	return this.items.pop();
+}
+
+let maze = new Maze(5,5);
